0e4e883a52d1a1236dbbc5bb4eee3e326c288a1e
Esteban <sistemas@electrobike.com.mx>
2018-01-23 11:51:51 -0600

    Reporte de EB
    Reporte Prueba 1

---
+++ sale_report/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sales Report',
+    'version': '1.1',
+    'category': 'Sales',
+    'summary': 'Sales internal machinery',
+    'description': """
+This module contains all the common features of Sales Management and eCommerce.
+    """,
+    'depends': ['sale','pos_sale'],
+    'data': [
+        'report/sale_report_views.xml'
+    ],
+    'demo': [
+        
+    ],
+    'uninstall_hook': "uninstall_hook",
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ sale_report/report/sale_report.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, tools
+
+
+class PosSaleReport_EB(models.Model):
+    _name = "report.all.channels.sales.EB"
+    _description = "All sales orders grouped by sales channels"
+    _auto = False
+
+    name = fields.Char('Order Reference', readonly=True)
+    partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
+    product_id = fields.Many2one('product.product', string='Product', readonly=True)
+    product_tmpl_id = fields.Many2one('product.template', 'Product Template', readonly=True)
+    date_order = fields.Datetime(string='Date Order', readonly=True)
+    user_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
+    categ_id = fields.Many2one('product.category', 'Product Category', readonly=True)
+    company_id = fields.Many2one('res.company', 'Company', readonly=True)
+    price_total = fields.Float('Total', readonly=True)
+    pricelist_id = fields.Many2one('product.pricelist', 'Pricelist', readonly=True)
+    country_id = fields.Many2one('res.country', 'Partner Country', readonly=True)
+    price_subtotal = fields.Float(string='Price Subtotal', readonly=True)
+    product_qty = fields.Float('Product Quantity', readonly=True)
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', readonly=True)
+    team_id = fields.Many2one('crm.team', 'Sales Channel', readonly=True)
+
+    def _so(self): ''' - Ventas - '''
+        so_str = """
+            WITH currency_rate as (%s)
+                SELECT sol.id AS id,
+                    so.name AS name,
+                    so.partner_id AS partner_id,
+                    sol.product_id AS product_id,
+                    pro.product_tmpl_id AS product_tmpl_id,
+                    so.date_order AS date_order,
+                    so.user_id AS user_id,
+                    pt.categ_id AS categ_id,
+                    so.company_id AS company_id,
+                    sol.price_total / COALESCE(cr.rate, 1.0) AS price_total,
+                    so.pricelist_id AS pricelist_id,
+                    rp.country_id AS country_id,
+                    sol.price_subtotal / COALESCE (cr.rate, 1.0) AS price_subtotal,
+                    (sol.product_uom_qty / u.factor * u2.factor) as product_qty,
+                    so.analytic_account_id AS analytic_account_id,
+                    so.team_id AS team_id
+
+            FROM sale_order_line sol
+                    JOIN sale_order so ON (sol.order_id = so.id)
+                    LEFT JOIN product_product pro ON (sol.product_id = pro.id)
+                    JOIN res_partner rp ON (so.partner_id = rp.id)
+                    LEFT JOIN product_template pt ON (pro.product_tmpl_id = pt.id)
+                    LEFT JOIN product_pricelist pp ON (so.pricelist_id = pp.id)
+                    LEFT JOIN currency_rate cr ON (cr.currency_id = pp.currency_id AND
+                        cr.company_id = so.company_id AND
+                        cr.date_start <= COALESCE(so.date_order, now()) AND
+                        (cr.date_end IS NULL OR cr.date_end > COALESCE(so.date_order, now())))
+                    LEFT JOIN product_uom u on (u.id=sol.product_uom)
+                    LEFT JOIN product_uom u2 on (u2.id=pt.uom_id)
+        """ % self.env['res.currency']._select_companies_rates()
+        return so_str
+
+    def _pos(self): ''' - Punto de venta - '''
+        pos_str = """
+                 SELECT
+                    (-1) * pol.id AS id,
+                    pos.name AS name,
+                    pos.partner_id AS partner_id,
+                    pol.product_id AS product_id,
+                    pro.product_tmpl_id AS product_tmpl_id,
+                    pos.date_order AS date_order,
+                    pos.user_id AS user_id,
+                    pt.categ_id AS categ_id,
+                    pos.company_id AS company_id,
+                    ((pol.qty * pol.price_unit) * (100 - pol.discount) / 100) AS price_total,
+                    pos.pricelist_id AS pricelist_id,
+                    rp.country_id AS country_id,
+                    (pol.qty * pol.price_unit) AS price_subtotal,
+                    (pol.qty * u.factor) AS product_qty,
+                    NULL AS analytic_account_id,
+                    config.crm_team_id AS team_id
+
+                FROM pos_order_line AS pol
+                    JOIN pos_order pos ON (pos.id = pol.order_id)
+                    LEFT JOIN pos_session session ON (session.id = pos.session_id)
+                    LEFT JOIN pos_config config ON (config.id = session.config_id)
+                    LEFT JOIN product_product pro ON (pol.product_id = pro.id)
+                    LEFT JOIN product_template pt ON (pro.product_tmpl_id = pt.id)
+                    LEFT JOIN product_category AS pc ON (pt.categ_id = pc.id)
+                    LEFT JOIN res_company AS rc ON (pos.company_id = rc.id)
+                    LEFT JOIN res_partner rp ON (rc.partner_id = rp.id)
+                    LEFT JOIN product_uom u ON (u.id=pt.uom_id)
+         """
+        return pos_str
+
+    def _from(self):
+        return """(%s UNION ALL %s)""" % (self._so(), self._pos())
+
+    def get_main_request(self):
+        request = """
+            CREATE or REPLACE VIEW %s AS
+                SELECT id AS id,
+                    name,
+                    partner_id,
+                    product_id,
+                    product_tmpl_id,
+                    date_order,
+                    user_id,
+                    categ_id,
+                    company_id,
+                    price_total,
+                    pricelist_id,
+                    analytic_account_id,
+                    country_id,
+                    team_id,
+                    price_subtotal,
+                    product_qty
+                FROM %s
+                AS foo""" % (self._table, self._from())
+        return request
+
+    @api.model_cr
+    def init(self):
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        self.env.cr.execute(self.get_main_request())
