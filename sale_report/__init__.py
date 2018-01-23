cf69326f7391f85675324a77b8da748e0f735cd4
Esteban <sistemas@electrobike.com.mx>
2018-01-23 12:03:52 -0600

    Actualizar init

--- sale_report/__init__.py
+++ sale_report/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import models
-from . import controllers
+#from . import models
+#from . import controllers
 from . import report
-from . import wizard
+#from . import wizard
 
 from functools import partial
 import odoo
