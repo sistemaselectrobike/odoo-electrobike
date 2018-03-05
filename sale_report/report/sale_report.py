c2529b3881e5e513ea8e49b34ae8abdc54a19be3
Cristina Ruiz <crizty.rz@hotmail.com>
2018-03-05 10:56:30 -0600

    Quitar notas canceladas

--- sale_report/report/sale_report.py
+++ sale_report/report/sale_report.py
@@ -58,6 +58,7 @@ class PosSaleReport_EB(models.Model):
                         (cr.date_end IS NULL OR cr.date_end > COALESCE(so.date_order, now())))
                     LEFT JOIN product_uom u on (u.id=sol.product_uom)
                     LEFT JOIN product_uom u2 on (u2.id=pt.uom_id)
+                    WHERE sale_order.state != 'cancel'
         """ % self.env['res.currency']._select_companies_rates()
         return so_str
 
